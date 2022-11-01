f414972faaae64046ed979324467f6e7cadc155f
alexander.laputko <alexander.laputko@ya.ru>
2022-11-01 21:34:26 +0300

    Add data about dogs in UI

--- app/src/main/java/com/alexa/woof/MainActivity.kt
+++ app/src/main/java/com/alexa/woof/MainActivity.kt
@@ -3,13 +3,23 @@ package com.alexa.woof
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.alexa.woof.data.Dog
+import com.alexa.woof.data.dogs
 import com.alexa.woof.ui.theme.WoofTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +27,61 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WoofTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                WoofApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun WoofApp() {
+    LazyColumn {
+        items(dogs) {
+            DogItem(dog = it)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    WoofTheme {
-        Greeting("Android")
+fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        DogIcon(dog.imageResourceId)
+        DogInformation(dog.name, dog.age)
+    }
+}
+
+@Composable
+fun DogIcon(@DrawableRes dogIcon: Int, modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier
+            .size(64.dp)
+            .padding(8.dp),
+        painter = painterResource(dogIcon),
+        contentDescription = null
+    )
+}
+
+@Composable
+fun DogInformation(@StringRes dogName: Int, dogAge: Int, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = stringResource(dogName),
+            modifier = modifier.padding(top = 8.dp)
+        )
+        Text(
+            text = stringResource(R.string.years_old, dogAge)
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun WoofPreview() {
+    WoofTheme(darkTheme = false) {
+        WoofApp()
     }
 }
