043d06678e4a0940366f65067133ac492533dad1
alexander.laputko <alexander.laputko@ya.ru>
2022-11-04 23:18:40 +0300

    Add light and dark Tob bar

--- app/src/main/java/com/alexa/woof/MainActivity.kt
+++ app/src/main/java/com/alexa/woof/MainActivity.kt
@@ -13,8 +13,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -39,9 +41,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WoofApp() {
-    LazyColumn(modifier = Modifier.background(MaterialTheme.colors.background)) {
-        items(dogs) {
-            DogItem(dog = it)
+    Scaffold(
+        topBar = { WoofTopAppBar() }
+    ) {
+        LazyColumn(modifier = Modifier.background(MaterialTheme.colors.background)) {
+            items(dogs) {
+                DogItem(dog = it)
+            }
         }
     }
 }
@@ -87,6 +93,28 @@ fun DogInformation(@StringRes dogName: Int, dogAge: Int, modifier: Modifier = Mo
         Text(
             text = stringResource(R.string.years_old, dogAge),
             style = MaterialTheme.typography.body1
+        )
+    }
+}
+
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.primary),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .size(64.dp)
+                .padding(8.dp),
+            painter = painterResource(id = R.drawable.ic_woof_logo),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(id = R.string.app_name),
+            style = MaterialTheme.typography.h1
         )
     }
 }
--- app/src/main/java/com/alexa/woof/data/Dog.kt
+++ app/src/main/java/com/alexa/woof/data/Dog.kt
@@ -20,5 +20,9 @@ val dogs = listOf(
     Dog(R.drawable.bella, R.string.dog_name_6, 14, R.string.dog_description_6),
     Dog(R.drawable.moana, R.string.dog_name_7, 2, R.string.dog_description_7),
     Dog(R.drawable.tzeitel, R.string.dog_name_8, 7, R.string.dog_description_8),
-    Dog(R.drawable.leroy, R.string.dog_name_9, 4, R.string.dog_description_9)
+    Dog(R.drawable.leroy, R.string.dog_name_9, 4, R.string.dog_description_9),
+    Dog(R.drawable.frankie, R.string.dog_name_3, 2, R.string.dog_description_3),
+    Dog(R.drawable.nox, R.string.dog_name_4, 8, R.string.dog_description_4),
+    Dog(R.drawable.faye, R.string.dog_name_5, 8, R.string.dog_description_5),
+    Dog(R.drawable.bella, R.string.dog_name_6, 14, R.string.dog_description_6),
 )
